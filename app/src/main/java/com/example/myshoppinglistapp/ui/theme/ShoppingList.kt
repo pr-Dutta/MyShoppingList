3c42bda6b461d2e62f502696a93075148bcf5d43
pr-Dutta <89duttapranjal@gmail.com>
2024-01-08 11:54:37 +0530

    first commit

---
+++ app/src/main/java/com/example/myshoppinglistapp/ui/theme/ShoppingList.kt
@@ -0,0 +1,94 @@
+package com.example.myshoppinglistapp.ui.theme
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+// (04-01-2024) -> have to revise it. - Done
+data class ShoppingItem(val id: Int,
+                        var name: String,
+                        var quantity: Int,
+                        var isEditing: Boolean = false
+)
+
+@Composable
+fun ShoppingListApp() {
+    // (04-01-2024) -> have to revise it. - Done
+    var sItems by remember{ mutableStateOf(listOf<ShoppingItem>()) }
+    // If the showDialog is false then the Dialog should be hide else shown
+    var showDialog by remember{ mutableStateOf(false) }
+    var itemName by remember{ mutableStateOf("") }
+    var itemQuantity by remember{ mutableStateOf("") }
+
+
+    // (03-01-2024)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { showDialog = true },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("Add Item")        //Lazy Column - Revised - Done
+
+            // 04-01-2024
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            /* The items function inside the LazyColumn is responsible
+             * for dynamically loading and rendering only the items that
+             * are currently visible on the screen*/
+            items(sItems) {
+
+            }
+        }
+    }
+
+
+    // (07-01-2024) - Have to revise
+    if (showDialog) {
+        AlertDialog(onDismissRequest = { showDialog = false },
+            confirmButton = { /*TODO*/ },
+            title = { Text("Add Shopping Item") },
+            text = {
+                Column {
+                    OutlinedTextField(
+                        value = itemName,
+                        onValueChange = { itemName = it },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth().padding(8.dp)
+                        )
+                    OutlinedTextField(
+                        value = itemQuantity,
+                        onValueChange = { itemQuantity = it },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth().padding(8.dp)
+                        )
+                    }
+                }
+            )
+    }
+}
+
+
+
