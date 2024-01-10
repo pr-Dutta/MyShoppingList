92e24b623e78888b13e4a3489ac6ceec1a060cee
pr-Dutta <89duttapranjal@gmail.com>
2024-01-10 12:20:17 +0530

    new commit

--- app/src/main/java/com/example/myshoppinglistapp/ui/theme/ShoppingList.kt
+++ app/src/main/java/com/example/myshoppinglistapp/ui/theme/ShoppingList.kt
@@ -1,5 +1,7 @@
 package com.example.myshoppinglistapp.ui.theme
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 // (04-01-2024) -> have to revise it. - Done
@@ -60,7 +64,7 @@ fun ShoppingListApp() {
              * for dynamically loading and rendering only the items that
              * are currently visible on the screen*/
             items(sItems) {
-
+                ShoppingListItem(it, {},{}) // (10-01-2024) What the hell is going on here just understood.
             }
         }
     }
@@ -120,6 +124,35 @@ fun ShoppingListApp() {
             )
     }
 }
+
+
+@Composable
+/* Here we can pass function and that function will be executed
+* when the ShoppingListItem() fun will call */
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,        //This is a lambda function - have to learn ask chatGpt?
+    onDeleteClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .border(
+                border = BorderStroke(2.dp, Color(0XFF018786)),
+                shape = RoundedCornerShape(20)
+            )
+    ) {
+        Text(text = item.name, Modifier.padding(8.dp))  // 8.dp are preferred to position items
+    }
+}
+
+
+
+
+
+
+
 
 
 
